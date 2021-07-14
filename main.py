588a7f5048ad655c3ad7cc502edab0027e863583
Fathan Akram <54970353+FathanAkram-app@users.noreply.github.com>
2021-07-14 13:14:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+
+import cv2
+import mediapipe as mp
+import pyautogui
+import PySimpleGUI as sg
+import Trackers.FaceTracker as FaceTracker
+
+USE_CAMERA = 0      # change to 1 for front facing camera
+window = sg.Window(
+  'DrawBody', 
+  [
+    [sg.Image(filename='', key='image')], 
+    [
+      sg.Button(
+      button_text="Face Tracker",),
+      sg.Button(
+      button_text="Hand Tracker"
+      ),
+      sg.Button(
+      button_text="Pose Tracker"
+      )
+    ]
+  ], 
+  location=(0, 0), 
+  grab_anywhere=True)
+
+cap = cv2.VideoCapture(USE_CAMERA)
+while window(timeout=20)[0] != sg.WIN_CLOSED:
+    window['image'](data=cv2.imencode('.png', FaceTracker.face_tracker(cap))[1].tobytes())
